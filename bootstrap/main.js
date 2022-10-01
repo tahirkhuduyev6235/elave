8b1c321d84b972a3c3fba4aee071e25bf7ee5412
tahirkhuduyev6235 <106512234+tahirkhuduyev6235@users.noreply.github.com>
2022-10-01 20:11:32 +0400

    Add files via upload

---
+++ bootstrap/main.js
@@ -0,0 +1,15 @@
+document.getElementById("first-btn").addEventListener(click, function() {
+    document.getElementById("first").scrollIntoView({ behaviour: 'smooth' })
+})
+document.getElementById("second-btn").addEventListener(click, function() {
+    document.getElementById("second").scrollIntoView({ behaviour: 'smooth' })
+})
+document.getElementById("third-btn").addEventListener(click, function() {
+    document.getElementById("third").scrollIntoView({ behaviour: 'smooth' })
+})
+document.getElementById("fourth-btn").addEventListener(click, function() {
+    document.getElementById("fourth").scrollIntoView({ behaviour: 'smooth' })
+})
+document.getElementById("fifth-btn").addEventListener(click, function() {
+    document.getElementById("fifth").scrollIntoView({ behaviour: 'smooth' })
+})
